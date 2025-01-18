232b274c89e8ba633beac0b74450f359355d2312
Stpv22 <145866689+STPv22@users.noreply.github.com>
2025-01-18 02:01:07 +0000

    idk know what im doin

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
-
+//https://www.youtube.com/watch?v=g2o22C3CRfU
 let getWwPercentage, getWhPercentage;
 let itemArr = [];
+let sorted = true;
+let solveInterval;
+document.querySelector('#isSortedElem').innerHTML = sorted;
+
 
 function setup() {
   createCanvas(windowWidth/100*79.2, windowHeight);
@@ -13,9 +17,8 @@ function setup() {
     return windowHeight/100*num;
   }
 
-
-  for (var i = 0; i < getWhPercentage(100); i++) {
-    var num = random(-150, 0);
+  for (var i = 0; i < 100; i++) {
+    var num = -getWhPercentage(i) - getWhPercentage(1);
     itemArr.push(num);
   }
 }
@@ -24,9 +27,69 @@ function draw() {
   background(0);
   noStroke();
 
-  //rect(0, windowHeight, 5, -150);
+  drawSort();
+}
 
+function drawSort() {
   for (var i = 0; i < getWwPercentage(100); i++) {
-    rect(i*5, windowHeight, 5, itemArr[i]);
+    //ill work this out
+    rect(i*getWwPercentage(0.79), windowHeight, getWwPercentage(0.79), itemArr[i]);
+  }
+}
+
+function desort() {
+  let currentIndex = itemArr.length;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [itemArr[currentIndex], itemArr[randomIndex]] = [
+      itemArr[randomIndex], itemArr[currentIndex]];
+  }
+  clear();
+  background(0);
+  sorted = false;
+  document.querySelector('#isSortedElem').innerHTML = sorted;
+  drawSort();
+}
+
+function sortCanvas() {
+  var sortTypeSelectE = document.querySelector('#sort-select');
+  var value = sortTypeSelectE.value;
+  document.querySelector('#sortbutton').disabled = true;
+
+  var sortednum = 0;
+  if (value == 'linear' && !sorted) {
+      solveInterval = setInterval(() => {
+        for (var i = 0; i < 100;) {
+          if (i === 99) {
+            sorted = true;
+            document.querySelector('#sortbutton').disabled = false;
+            clearInterval(solveInterval);
+          }
+          if (-itemArr[i] < -itemArr[i+1]) {
+            i++;
+          } else {
+            break;
+          }
+        }
+        for (var i = 0; i < 100; i++) {
+          if (-itemArr[i] > -itemArr[i+1]) {
+            [itemArr[i], itemArr[i+1]] = [
+              itemArr[i+1], itemArr[i]];
+          }
+        }
+
+        clear();
+        background(0);
+        document.querySelector('#isSortedElem').innerHTML = sorted;
+        drawSort();
+      }, 50);
+  } else {
+    console.log('wip');
   }
 }
